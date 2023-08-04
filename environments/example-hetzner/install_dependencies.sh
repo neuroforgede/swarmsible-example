5e855bfe5cd6f64767c956875614f06b789381b0
Martin Braun <braun@neuroforge.de>
2023-08-04 23:41:46 +0200

    update swarmsible example-hetzner to work with arm servers

--- environments/example-hetzner/install_dependencies.sh
+++ environments/example-hetzner/install_dependencies.sh
@@ -6,10 +6,10 @@ pip3 install ansible==6.5.0
 pip3 install hcloud==1.18.0
 pip3 install yq
 pip3 install https://github.com/neuroforgede/docker-stack-deploy/archive/refs/tags/0.2.3.zip
-ansible-galaxy collection install hetzner.hcloud:==1.8.2
+ansible-galaxy collection install hetzner.hcloud:==2.0.0
 
 rm -rf swarmsible
 mkdir -p swarmsible
 cd swarmsible
-git clone --single-branch --branch 22.10.0.beta https://github.com/neuroforgede/swarmsible.git
-git clone --single-branch --branch 22.10.0.beta https://github.com/neuroforgede/swarmsible-hetzner.git
+git clone --single-branch --branch 23.06.0 https://github.com/neuroforgede/swarmsible.git
+git clone --single-branch --branch 23.02.1 https://github.com/neuroforgede/swarmsible-hetzner.git
